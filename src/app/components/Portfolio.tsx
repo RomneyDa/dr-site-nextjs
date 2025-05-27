2cfee66c7a3b86db93c893a7f3ae751dbe2fe38f
Dallin Romney <dallinromney@gmail.com>
2025-05-27 00:51:05 -0700

    move images and resume to be in repo

--- src/app/components/Portfolio.tsx
+++ src/app/components/Portfolio.tsx
@@ -1,33 +1,43 @@
-"use client"
-import React from 'react'
-import { PORTFOLIO_ITEMS, PortfolioItemProps, s3FileUrl } from '../content'
-import Link from 'next/link'
-import Image from 'next/image'
+"use client";
+import React from "react";
+import { PORTFOLIO_ITEMS, PortfolioItemProps } from "../content";
+import Link from "next/link";
+import Image from "next/image";
 
-const PortfolioItem = ({ title, description, image, link }: PortfolioItemProps) => {
-    return (
-        <Link
-            target='_blank'
-            href={link}
-            className="w-full aspect-[1.2] relative overflow-hidden rounded-2xl bg-background hover:scale-105 border hover:ring-2 ring-ring transition-all duration-500 cursor-pointer"
-        >
-            <Image width={350} height={250} src={s3FileUrl(image)} alt={title} className="object-cover w-full h-full" />
-            <div className="p-4 absolute bottom-0 left-0 w-full max-h-1/2 bg-background/85">
-                <h3 className="text-lg font-bold">{title}</h3>
-                <p>{description}</p>
-            </div>
-        </Link>
-    )
-
-}
+const PortfolioItem = ({
+  title,
+  description,
+  image,
+  link,
+}: PortfolioItemProps) => {
+  return (
+    <Link
+      target="_blank"
+      href={link}
+      className="w-full aspect-[1.2] relative overflow-hidden rounded-2xl bg-background hover:scale-105 border hover:ring-2 ring-ring transition-all duration-500 cursor-pointer"
+    >
+      <Image
+        width={350}
+        height={250}
+        src={`/portfolio/${image}`}
+        alt={title}
+        className="object-cover w-full h-full"
+      />
+      <div className="p-4 absolute bottom-0 left-0 w-full max-h-1/2 bg-background/85">
+        <h3 className="text-lg font-bold">{title}</h3>
+        <p>{description}</p>
+      </div>
+    </Link>
+  );
+};
 const Portoflio = () => {
-    return (
-        <div className="w-[90%] max-w-[350px] sm:max-w-[800px] lg:max-w-[1100px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            {PORTFOLIO_ITEMS.map((item, index) => (
-                <PortfolioItem key={index} {...item} />
-            ))}
-        </div>
-    )
-}
+  return (
+    <div className="w-[90%] max-w-[350px] sm:max-w-[800px] lg:max-w-[1100px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+      {PORTFOLIO_ITEMS.map((item, index) => (
+        <PortfolioItem key={index} {...item} />
+      ))}
+    </div>
+  );
+};
 
-export default Portoflio
+export default Portoflio;
--- src/app/content/index.ts
+++ src/app/content/index.ts
@@ -1,57 +1,69 @@
+export const THEME_COLOR = "#121621"; // rgb(18, 22, 33) // hsl(224, 29%, 10%)
 
-export const THEME_COLOR = "#121621" // rgb(18, 22, 33) // hsl(224, 29%, 10%)
+// export const S3_BUCKET_URL =
+//   "https://s3.us-west-1.amazonaws.com/dallinromney.com/";
 
-export const S3_BUCKET_URL = "https://s3.us-west-1.amazonaws.com/dallinromney.com/"
+// export const s3FileUrl = (file: string) => `${S3_BUCKET_URL}${file}`;
 
-export const s3FileUrl = (file: string) => `${S3_BUCKET_URL}${file}`
+// export const RESUME_LINK = s3FileUrl("resume-dallin-romney.pdf");
 
-export const RESUME_LINK = s3FileUrl("resume-dallin-romney.pdf")
+export const RESUME_LINK = "/resume.pdf";
 
-export const CONTACT_LINK = "mailto:dallin@thunklab.com"
+export const CONTACT_LINK = "mailto:dallin@thunklab.com";
 
 export interface PortfolioItemProps {
-    title: string
-    description: string
-    image: string
-    link: string
+  title: string;
+  description: string;
+  image: string;
+  link: string;
 }
 
 export const PORTFOLIO_ITEMS: PortfolioItemProps[] = [
-    {
-        title: "MapThis.AI",
-        description: "AI map generation tools to auto-generate, edit, and serve maps from web content",
-        image: "portfolio/mapthis.png",
-        link: "https://mapthis.ai"
-    },
-    {
-        title: "OutbackFriends.com",
-        description: "Social network of friends that help each other with tasks",
-        image: "portfolio/outback-friends.png",
-        link: "https://outbackfriends.com"
-    },
-    {
-        title: "MusicStreet.xyz",
-        description: "Learning Tracking app with NFC-tap functionality",
-        image: "portfolio/music-street.png",
-        link: "https://musicstreet.xyz"
-    },
-    {
-        title: "Gear Generation",
-        description: "Dynamic, animatable spur gear/gear set svg generation packages",
-        image: "portfolio/gears.png",
-        link: "https://github.com/RomneyDa/react-gear-gen"
-    },
+  {
+    title: "Continue.dev",
+    description:
+      "The leading open-source AI Coding Assistant for VS Code and Jetbrains",
+    image: "continue_agent.gif",
+    link: "https://continue.dev",
+  },
+  {
+    title: "MapThis.AI",
+    description:
+      "AI map generation tools to auto-generate, edit, and serve maps from web content",
+    image: "map_this.webp",
+    link: "https://mapthis.ai",
+  },
+  {
+    title: "OutbackFriends.com",
+    description: "Social network of friends that help each other with tasks",
+    image: "outback_friends.webp",
+    link: "https://outbackfriends.com",
+  },
+  {
+    title: "MusicStreet.xyz",
+    description: "Learning Tracking app with NFC-tap functionality",
+    image: "music_street.webp",
+    link: "https://musicstreet.xyz",
+  },
+  {
+    title: "Gear Generation",
+    description:
+      "Dynamic, animatable spur gear/gear set svg generation packages",
+    image: "gear_gen.webp",
+    link: "https://github.com/RomneyDa/react-gear-gen",
+  },
 
-    {
-        title: "rpg-gpt.com",
-        description: "Frontend for text-based AI role-playing game built around OpenAI",
-        image: "portfolio/rpggpt.png",
-        link: "https://rpg-gpt.com"
-    },
-    {
-        title: "Popchew",
-        description: "Online ordering system and mobile application for virtual food brand platform. Typescript, React Native iOS/Android, Python, Animations, AWS, Analytics",
-        image: "portfolio/popchew.png",
-        link: "https://popchew.com"
-    }
-]
+  {
+    title: "rpg-gpt.com",
+    description:
+      "Frontend for text-based AI role-playing game built around OpenAI",
+    image: "rpg_gpt.webp",
+    link: "https://rpg-gpt.com",
+  },
+  // {
+  //     title: "Popchew",
+  //     description: "Online ordering system and mobile application for virtual food brand platform. Typescript, React Native iOS/Android, Python, Animations, AWS, Analytics",
+  //     image: "portfolio/popchew.png",
+  //     link: "https://popchew.com"
+  // }
+];
